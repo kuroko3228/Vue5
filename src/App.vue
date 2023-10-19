d2e1aa2f828bde91ea8b0786bb3dabe222a8b0f7
Ешенкулов Олжас Ермекович <EshenkulovO@lcoast.itstep>
2023-10-19 15:59:31 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data: () => {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+<template>
+  <div class="app">app</div>
+</template>
+
+<style scoped></style>
